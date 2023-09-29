86cadcd72df7dc4f364fb277d0c63b57c5cdedeb
Lucja Zawiasa <zawiasalucy@gmail.com>
2023-09-29 15:43:28 +0200

    Klar med hela PetStore

--- Grundövningar/Classes/Dog.cs
+++ Grundövningar/Classes/Dog.cs
@@ -3,13 +3,13 @@ using Grundövningar.Interfaces;
 
 namespace Grundövningar.Classes
 {
-	public class Cat : IPet
+	public class Dog : IPet
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public Animal Animal { get; set; }
 		public string Feed { get; set; }
 
-		public string Eat() => $"Chewing on a {Feed}";
+		public string Eat() => $"Munching on {Feed}";
 	}
 }
---
+++ Grundövningar/Classes/PetStore.cs
@@ -0,0 +1,30 @@
+﻿using Grundövningar.Interfaces;
+using System.Net.Http.Json;
+
+namespace Grundövningar.Classes
+{
+	public class PetStore
+	{
+		HttpClient _http;
+        public PetStore(HttpClient http) => _http = http;
+
+        public string Message { get; private set; } = string.Empty;
+		public List<IPet> Pets { get; private set; } = new();
+
+		public async Task<List<IPet>> LoadPets()
+		{
+			var cats = await _http.GetFromJsonAsync<List<Cat>>("sample-data/cat.json");
+			var dogs = await _http.GetFromJsonAsync<List<Dog>>("sample-data/dog.json");
+			if (cats is not null)  Pets.AddRange(cats);
+			if (dogs is not null)  Pets.AddRange(dogs);
+			return Pets;
+
+		}
+		public void Feed(int id)
+		{
+            var pet = Pets.FirstOrDefault(p => p.Id == id);
+            Message = pet?.Eat() ?? "Not hungry";
+        }
+
+    }
+}
